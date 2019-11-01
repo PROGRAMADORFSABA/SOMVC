435527ebf2901512285401d1fcf28ea81bea8b21
Alex Nogueira <tec.alexnogueira@gmail.com>
2019-11-01 17:43:51 -0300

    Mudanças em clienteFaltaLicitacao, Services, Controller, DAO + autocomplete

--- App/Views/pedidofalta/cadastro.php
+++ App/Views/pedidofalta/cadastro.php
@@ -220,7 +220,7 @@
                                                     <tr>
                                                         <td>
                                                             <?php echo $produto->getProNome(); ?>
-                                                            <input type="hidden" name="produtos[]" value=<?php echo $produto->getProCodigo(); ?> >
+                                                            <input type="hidden" name="produtos[]" value=<?php echo $produto->getIdProduto(); ?> >
                                                         </td>
                                                         <td><button class="btn btn-danger btn-sm" type="button" onClick="app.removeProduto(this,<?php echo $produto->getProCodigo(); ?>)">remover</button></td>
                                                     </tr>
