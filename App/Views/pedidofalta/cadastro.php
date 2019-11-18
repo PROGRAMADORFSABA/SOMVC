5a3e0f3c10efd6acafaf2f9419d20a72ae1a0025
Alex Nogueira <tec.alexnogueira@gmail.com>
2019-11-17 21:07:33 -0300

    cadastro pedidofalta

--- App/Views/pedidofalta/cadastro.php
+++ App/Views/pedidofalta/cadastro.php
@@ -1,157 +1,151 @@
 <!-- begin:: Content -->
-<div class="kt-content  kt-grid__item kt-grid__item--fluid" id="kt_content">
 
-    <section id="main-content">
-        <section class="wrapper">
-            <div class="row">
-                
-                <?php
-                    if($Sessao::retornaErro()){ ?>
-                    <div class="col-lg-12">
-                        <div class="alert alert-danger" role="alert">
-                            <?php 
-                                foreach($Sessao::retornaErro() as $key => $mensagem){ ?>
-                                <?php echo $mensagem; ?> <br>
-                            <?php } ?>
-                        </div>
+<div class="kt-portlet">
+    <div class="kt-portlet__head">
+        <div class="kt-portlet__head-label">
+            <h3 class="kt-portlet__head-title">
+                CADASTRO DE FALTA
+            </h3>
+        </div>
+    </div>
+
+    <!--begin::Form-->
+    <form class="kt-form kt-form--label-right"  method="POST" action="http://<?php echo APP_HOST; ?>/pedidofalta/salvar" >
+        <div class="kt-portlet__body">
+            <section class="panel panel-default">
+            <div class="form-group row form-group-marginless kt-margin-t-20">
+                <label class="col-lg-1 col-form-label">Cliente:</label>
+                <div class="col-lg-3">
+                    <input type="text" name="autocompleteCliente" id="autocomplete-clientefalta" class="form-control" placeholder="Cliente" value=<?php echo $viewVar['pedidofalta']->getFkCliente()->getNomeFantasia();?>>
+                    <input type="hidden" id="cliente" name="cliente" value=<?php echo $viewVar['pedidofalta']->getFkCLiente()->getCodCliente()?> >
+                    <span class="form-text text-muted">Pro favor digite um nomde de Cliente</span>
+                </div>
+                <label class="col-lg-1 col-form-label">Proposta:</label>
+                <div class="col-lg-3">
+                    <input type="text" class="form-control" placeholder="Proposta" required value="<?php echo $Sessao::retornaValorFormulario('proposta'); ?>">
+                    <span class="form-text text-muted">Por favor, digite o Nº da proposta</span>
+                </div>
+                <label class="col-lg-1 col-form-label">AFM:</label>
+                <div class="col-lg-3">
+                    <div class="input-group">
+                        <div class="input-group-prepend"><span class="input-group-text"><i class="flaticon2-delivery-package"></i></span></div>
+                        <input type="text" class="form-control" id="afm" name="afm" placeholder="AFM"  required value= "<?php echo $Sessao::retornaValorFormulario('afm'); ?>">
                     </div>
-                <?php } ?>
-			<div class=" col-lg-4 col-md-9 col-sm-12">
-				
-				</div>
-			</div>
-		<!--begin::Modal-->
-		<div class="modal fade" id="kt_select2_modal" role="dialog" aria-labelledby="" aria-hidden="true">
-				
-				</div>
-        <!--end::Modal-->
-               <form class="form-horizontal" id="form_cadastro_produto" method="POST" action="http://<?php echo APP_HOST; ?>/pedidofalta/salvar">
-                    <div class="col-lg-8">
-                        <section class="panel panel-default">
-                            <header class="panel-heading">
-                                Cadastro de Falta
-                            </header>
-                            <div class="panel-body">
-                                <div class="form-group">
-                                    <label  class="col-lg-3 col-sm-3 control-label">Proposta</label>
-                                    <div class="col-lg-9">
-                                        <div class="iconic-input right">
-                                            <input type="text" id="proposta" name="proposta" class="form-control" placeholder="Proposta"
-                                                   required value="<?php echo $Sessao::retornaValorFormulario('proposta'); ?>">
-                                        </div>
-                                    </div>
-                                </div>
-                              
-
-                                <div class="form-group">
-                                    <label  class="col-lg-3 col-sm-3 control-label">AFM</label>
-                                    <div class="col-lg-9">
-                                        <div class="iconic-input right">
-                                            <input type="text" id="afm" name="afm" class="form-control" placeholder="Proposta"
-                                                   required value= "<?php   echo $Sessao::retornaValorFormulario('afm'); ?>" >
-                                        </div>
-                                    </div>
-                                </div>
-                                <!--div class="form-group">
-                                    <label  class="col-lg-3 col-sm-3 control-label">Data Cadastro</label>
-                                    <div class="col-lg-9">
-                                        <div class="iconic-input right">
-                                            <input type="text" name="proposta" class="form-control" placeholder="Data Cadastro"
-                                                   required value= >
-                                                   
-                                        </div>
-                                    </div>
-                                </div-->
-                                <div class="form-group">
-                                    <label  class="col-lg-3 col-sm-3 control-label">Cliente</label>
-                                    <div class="col-lg-9">
-                                        <div>
-                                            <input type="text" name="autocompleteCliente" id="autocomplete-clientefalta" class="form-control" required placeholder="Cliente - autocomplete"
-                                                   value=<?php echo $viewVar['pedidofalta']->getFkCliente()->getNomeFantasia();?> >
-
-                                            <input type="hidden" id="cliente" name="cliente"
-                                                   value=<?php echo $viewVar['pedidofalta']->getFkCLiente()->getCodCliente()?> >
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label  class="col-lg-3 col-sm-3 control-label">Observação</label>
-                                    <div class="col-lg-9">
-                                        <div class="iconic-input right">
-                                            <textarea maxlength="350" class="form-control spinner" placeholder="Observacao da falta" rows="5" name="observacao" value="<?php echo $Sessao::retornaValorFormulario('observacao'); ?>" ></textarea>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </section>
-                    </div>
-                    <div class="col-md-12">
-                        <section class="panel panel-default">
-                            <header class="panel-heading">
-                                Produtos
-                            </header>
-                            <div class="panel-body">
-                                <div id="produto-selecionados">
-                                    <div class="form-group">
-                                        <label  class="col-lg-3 col-sm-3 control-label">Adicionar</label>
-                                        <div class="col-lg-9">
-                                            <div class="iconic-input right">
-                                                <input type="text" id="autocomplete-produto" class="form-control " placeholder="Produto">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <table class="table table-striped">
-                                        <thead>
-                                        <th>Produto</th>
-                                        <th>Remover</th>
-                                        </thead>
-                                        <tbody id="editar-tabela-produtos">
-                                        <?php
-                                            if($viewVar['pedidofalta']->getFk_Produto()) {
-                                                foreach ($viewVar['pedidofalta']->getFk_Produto() as $produto) {
-                                                    ?>
-                                                    <tr>
-                                                        <td>
-                                                            <?php echo $produto->getProNome(); ?>
-                                                            <input type="hidden" name="produtos[]" value=<?php echo $produto->getProCodigo(); ?> >
-                                                        </td>
-                                                        <td><button class="btn btn-danger btn-sm" type="button" onClick="app.removeProduto(this,<?php echo $produto->getProCodigo(); ?>)">remover</button></td>
-                                                    </tr>
-                                                    <?php
-                                                }
-                                            }
-                                        ?>
-                                        </tbody>
-                                    </table>
-
-                                </div>
-                        </section>
-                    </div>
-                    <div class="col-md-12">
-                        <div align="right">
-                            <a class="btn btn-success" href="http://<?php echo APP_HOST; ?>/pedidofalta/index">Voltar</a>
-                            <button class="btn btn-info">Cadastrar</button>
-                        </div>
-                    </div>
-
-                </form>
+                    <span class="form-text text-muted">Por favor, digite a AFM</span>
+                </div>
             </div>
-        </section>
-    </section>
-    <div id="modal-produtos" class="modal fade" role="dialog">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title">ATENÇÃO</h4>
+            <div class="kt-separator kt-separator--border-dashed kt-separator--space-lg kt-separator--portlet-fit"></div>
+            <div class="form-group row form-group-marginless">
+                <label class="col-lg-1 col-form-label">Status:</label>
+                <div class="col-lg-3">
+                    <select type="email" class="form-control" placeholder="Status"></select>
+                    <span class="form-text text-muted">Por favor, escolha o status da falta.</span>
                 </div>
-                <div class="modal-body">
-                    <div class="alert alert-block alert-danger fade in" id="div-modal">
+                <label class="col-lg-1 col-form-label">Observação:</label>
+                <div class="col-lg-3">
+                    <div class="kt-input-icon">
+                        <textarea maxlength="350" type="text" class="form-control spinner" placeholder="Observacao da falta" rows="5" name="observacao" value="<?php echo $Sessao::retornaValorFormulario('observacao'); ?>"></textarea>
+                        <span class="kt-input-icon__icon kt-input-icon__icon--right"><span><i class="la la-info-circle"></i></span>
                     </div>
+                    <span class="form-text text-muted">Please enter fax</span>
                 </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+                <label class="col-lg-1 col-form-label">Address:</label>
+                <div class="col-lg-3">
+                    <div class="kt-input-icon">
+                        <input type="text" class="form-control" placeholder="Enter your address">
+                        <span class="kt-input-icon__icon kt-input-icon__icon--right"><span><i class="la la-map-marker"></i></span></span>
+                    </div>
+                    <span class="form-text text-muted">Please enter your address</span>
+                </div>
+            </div>
+            <div class="kt-separator kt-separator--border-dashed kt-separator--space-lg kt-separator--portlet-fit"></div>
+            <div class="form-group row">
+                <label class="col-lg-1 col-form-label">Produto:</label>
+                <div class="col-lg-3">
+                    <div class="kt-input-icon">
+                        <input type="text" id="autocomplete-produto" class="form-control" placeholder="Auto - complete Produto">
+                        <span class="kt-input-icon__icon kt-input-icon__icon--right"><span><i class="fa fa-plus-circle"></i></span></span>
+                    </div>
+                    <span class="form-text text-muted">Por favor, digite o nome do produto.</span>
+                    <table class="table table-striped">
+                        <thead>
+                        <th>Produto</th>
+                        <th>Remover</th>
+                        </thead>
+                        <tbody id="editar-tabela-produtos">
+                        <?php
+                            if($viewVar['pedidofalta']->getFk_Produto()) {
+                                foreach ($viewVar['pedidofalta']->getFk_Produto() as $produto) {
+                                    ?>
+                                    <tr>
+                                        <td>
+                                            <?php echo $produto->getProNome(); ?>
+                                            <input type="hidden" name="produtos[]" value=<?php echo $produto->getProCodigo(); ?> >
+                                        </td>
+                                        <td><button class="btn btn-danger btn-sm" type="button" onClick="app.removeProduto(this,<?php echo $produto->getProCodigo(); ?>)">remover</button></td>
+                                    </tr>
+                                    <?php
+                                }
+                            }
+                        ?>
+                        </tbody>
+                    </table>
+                </div>
+                <label class="col-lg-1 col-form-label">Tipo de Licitação:</label>
+                <div class="col-lg-3">
+                    <div class="kt-radio-inline">
+                        <label class="kt-radio kt-radio--solid">
+                            <input type="radio" name="example_2" checked value="2"> Presencial
+                            <span></span>
+                        </label>
+                        <label class="kt-radio kt-radio--solid">
+                            <input type="radio" name="example_2" value="2"> Eletronico
+                            <span></span>
+                        </label>
+                    </div>
+                    <span class="form-text text-muted">Por favor, selecione o tipo de pregão.</span>
                 </div>
             </div>
         </div>
-    </div>
+        <div class="kt-portlet__foot">
+            <div class="kt-form__actions">
+                <div class="row">
+                    <div class="col-lg-5"></div>
+                    <div class="col-lg-7">
+                        <button class="btn btn-brand">Enviar</button>
+                        <a href="http://<?php echo APP_HOST; ?>/pedidofalta/index" class="btn btn-secondary">Cancel</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="modal-produtos" class="modal fade" role="dialog">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title">ATENÇÃO</h4>
+                    </div>
+                    <div class="modal-body">
+                        <div class="alert alert-block alert-danger fade in" id="div-modal">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+    </section>
+    </section>
+
+    <!--end::Form-->
+</div>
+
+<!--end::Portlet-->
+</div>
+</div>
+</div>
+
+<!-- end:: Content -->
+</div>
